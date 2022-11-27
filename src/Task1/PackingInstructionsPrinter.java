37cf5a2b5fdb6f48b5e7867a11e28efde85c41d7
tbecke17 <103056362+tbecke17@users.noreply.github.com>
2022-11-27 22:44:32 +0100

    Update PackingInstructionsPrinter.java

--- src/Task1/PackingInstructionsPrinter.java
+++ src/Task1/PackingInstructionsPrinter.java
@@ -8,8 +8,8 @@ public class PackingInstructionsPrinter implements Visitor {
 	public void visitGlass(Glass g) {
 
 		System.out.println(g.getName() + ":");
-		System.out.println(">should be wrapped with Bubble wraps and packed in a box with dimension: " + g.getHeight()
-				+ 1 + "x" + g.getWidth() + 1 + "x" + g.getLenght() + 1);
+		System.out.println(">should be wrapped with Bubble wraps and packed in a box with dimension: " + (g.getHeight()
+				+ 1) + "x" + (g.getWidth() + 1) + "x" + (g.getLenght() + 1));
 
 	}
 
@@ -25,7 +25,7 @@ public class PackingInstructionsPrinter implements Visitor {
 	public void visitElectronic(Electronic e) {
 		System.out.println(e.getName() + ":");
 		System.out.println(">should be covered with Polyethylene foam film and packed in a box with dimension: "
-				+ e.getHeight() + 1 + "x" + e.getWidth() + 1 + "x" + e.getLenght() + 1);
+				+ (e.getHeight() + 1) + "x" + (e.getWidth() + 1) + "x" + (e.getLenght() + 1));
 
 	}
 
